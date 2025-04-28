3bae5f057d7ddb201a4e93dd8ed3ae5e4844464e
Joseph <Jaghoghov@gmail.com>
2025-04-28 18:00:10 +0100

    restructure: added noisetorch file

---
+++ modules/services/sound/noisetorch.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  ###### interface
+  options = {
+    noisetorch.enable = lib.mkEnableOption "noisetorch";
+  };
+
+  config = lib.mkIf config.noisetorch.enable {
+    environment.systemPackages = [ pkgs.noisetorch ]; # Make sure this is a list
+    programs.noisetorch.enable = true;
+
+  };
+
+}
