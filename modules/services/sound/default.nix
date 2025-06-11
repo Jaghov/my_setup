de85a1c90c3aedc146e7e6e715e5695b76ff5e8f
Joseph <Jaghoghov@gmail.com>
2025-06-11 12:11:48 +0100

    feat: removed automatic volume control

--- modules/services/sound/default.nix
+++ modules/services/sound/default.nix
@@ -25,7 +25,24 @@
 
       # use the example session manager (no others are packaged yet so this is enabled by default,
       # no need to redefine it in your config for now)
-      wireplumber.enable = true;
+      wireplumber = {
+        enable = true;
+        extraConfig = {
+          access.rules = [
+          {
+            # Removes electron apps' ability to change the mic gain by themselves.
+            # check https://pipewire.pages.freedesktop.org/wireplumber/daemon/configuration.html for more info
+            # and https://www.reddit.com/r/archlinux/comments/190dvl8/pipewirewayland_how_to_stop_applications_from/ for the source
+            # of this fix
+            matches = [ { application.process.binary = "electron"; } ];
+            actions = { update-props = { default_permissions = "rx"; }; };
+          }
+            
+            
+          ];
+        };
+        
+      };
     };
     environment.systemPackages = lib.mkIf config.vol_control.enable [ pkgs.pavucontrol ];
     
